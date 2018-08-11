ef4b1fadd89acb192e719a49e4ad020161576e34
Emna Guellaty <eguellaty@gmail.com>
2018-08-11 13:25:38 +0100

    new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   include Pundit
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # Pundit: white-list approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
@@ -21,6 +22,11 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :photo, :photo_cache])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:email, :photo, :photo_cache])
+    end
 end
 
 
