fc1f2ee43940c23ea93afcfe8ea98bd8bc1096a9
Emna Guellaty <eguellaty@gmail.com>
2018-08-12 01:43:34 +0100

    work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,15 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
     @boats = Boat.first(3)
+    @boats = Boat.where.not(latitude: nil, longitude: nil)
+
+    @markers = @boats.map do |boat|
+      {
+        lat: boat.latitude,
+        lng: boat.longitude#,
+        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+      }
+    end
   end
 
   def dashboard
